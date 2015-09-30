7ad3712fe67d31783e42610178f71e4414564172
Charles <euromkay@gmail.com>
2015-09-29 18:06:36 -0700

    ip update
    run

--- tron_runner.py
+++ tron_runner.py
@@ -11,14 +11,14 @@ if local:
 	scale = 7
 
 	Process(target = tron_master.start, args = [ip, port]).start()
-	time.sleep(1)
+	time.sleep(2)
 	for x in range(5):
 		for y in range (3):
 			coords = (x * 300, y * 200)
 			Process(target = tron_render.display, args=[ip, port, x, y, scale, coords]).start()
 
 else:
-	ip = '10.10.0.255'
+	ip = '10.10.0.10'
 	port = 5000
 	scale = 30
 	name = platform.node()
